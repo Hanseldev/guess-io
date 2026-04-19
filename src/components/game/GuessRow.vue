d0cb7ae32d56aa490fbc2e322fe0b031e4499838
Hansel <hauwmgames@gmail.com>
2026-04-19 14:24:50 +0100

    feat: implement GuessRow component with dynamic code length based on game difficulty

--- src/components/game/GuessRow.vue
+++ src/components/game/GuessRow.vue
@@ -1,5 +1,24 @@
-<!-- <template>
-    <div v-for="value in source">
-
+<template>
+	<div v-for="value in codeLength" :key="value">
+        <NumberTile
+            v-model="value"
+            :disabled="true"
+            :isFocused="false"
+            status="pending">
+        </NumberTile>
     </div>
-</template> -->
+</template>
+
+<script setup lang="ts">
+	import { GAME_MODES } from "@/constants/gameConfig";
+	import { storeToRefs } from "pinia";
+	import { useGameStore } from "@/stores/gameStore";
+	import { computed } from "vue";
+    import NumberTile from "./NumberTile.vue";
+
+	const store = useGameStore();
+	const { difficulty } = storeToRefs(store);
+
+	const codeLength = computed(() => GAME_MODES[difficulty.value].codeLength);
+	
+</script>
---
+++ src/views/GameView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <GuessRow></GuessRow>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import GuessRow from "@/components/game/GuessRow.vue";
+
+</script>
